8b148582b9c2000f7d1adb6833e9f3afdf935e92
Rachael Gyles <rgyles@ga-c1.21-school.ru>
2019-11-10 17:07:23 +0300

    first version of arena output, added vim .swp ignore

--- virtual_machine/src/arena.c
+++ virtual_machine/src/arena.c
@@ -6,7 +6,7 @@
 /*   By: rgyles <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 15:09:32 by rgyles            #+#    #+#             */
-/*   Updated: 2019/11/08 20:11:27 by rgyles           ###   ########.fr       */
+/*   Updated: 2019/11/10 17:05:40 by rgyles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,74 +14,114 @@
 #include "op.h"
 #include "libft.h"
 
-void	print_cell(char *cell)
+void	print_bit(unsigned char bit)
 {
-	int i;
-	unsigned char	symbol;
-
-	i = 0;
-	symbol = *cell;
-	while (i < 2)
-	{
-		if (symbol < 10)
-			ft_putchar(symbol % 10 + 48);
-		else if (symbol == 10)
-			ft_putchar('a');
-		else if (symbol == 11)
-			ft_putchar('b');
-		else if (symbol == 12)
-			ft_putchar('c');
-		else if (symbol == 13)
-			ft_putchar('d');
-		else if (symbol == 14)
-			ft_putchar('e');
-		else if (symbol == 15)
-			ft_putchar('f');
-		++i;
-		symbol = *(cell + 1);
-	}
-	ft_putchar(' ');
+	if (bit < 10)
+		ft_putchar(bit + 48);
+	else if (bit == 10)
+		ft_putchar('a');
+	else if (bit == 11)
+		ft_putchar('b');
+	else if (bit == 12)
+		ft_putchar('c');
+	else if (bit == 13)
+		ft_putchar('d');
+	else if (bit == 14)
+		ft_putchar('e');
+	else if (bit == 15)
+		ft_putchar('f');
 }
 
-void	print_arena(unsigned short int *arena)
+void	print_arena_row(unsigned char *arena, int row)
 {
-	int	cell;
-	int	row;
+	int i;
+	int	total;
 
-	cell = 0;
-	row = 32;
-	printf("cell - %hx\n", arena[0]);
-	while (cell < row)
+	total = (row + 1) * 32;
+	i = total - 32;
+	print_bit((arena[i] & 0xf) >> 4);
+	print_bit(arena[i] & 0xf);
+	while (i++ < total)
 	{
-		//printf("%hx ", arena[cell]);
-		print_cell((char *)&arena[cell]);
-		++cell;
+		ft_putchar(' ');
+		print_bit((arena[i] & 0xf0) >> 4);
+		print_bit(arena[i] & 0xf);
 	}
+}
+
+void	print_row_label(unsigned short int row_label)
+{
+	unsigned char	*label;
+
+	label = (unsigned char *)&row_label;
+	ft_putstr("0x");
+	print_bit((*(label + 1) & 0xf0) >> 4);
+	print_bit(*(label + 1) & 0xf);
+	print_bit((*label & 0xf0) >> 4);
+	print_bit(*label & 0xf);
+	//printf("row_label - %d\n", row_label);
+	//printf("label - %d\n", *(label + 0));
+	ft_putstr(": ");
+}
+
+void	print_arena(unsigned char *arena)
+{
+	int	row;
+	int	total;
+	unsigned short int	row_label;
+
+	row = 0;
+	total = MEM_SIZE / 32 - 1;
+	//total = 2;
+	row_label = 0;
+	print_row_label(row_label);
+	row_label += 32;
+	print_arena_row(arena, row);
+	ft_putchar('\n');
+	while (row++ < total)
+	{
+		print_row_label(row_label);
+		row_label += 32;
+		print_arena_row(arena, row);
+		ft_putchar('\n');
+	}
+	//printf("cell - %hx\n", arena[0]);
+	//print_arena_row(arena, 0);
+	/*while (i < row)
+	{
+		print_bit((arena[cell] & 0xf) >> 4);
+		print_bit(arena[cell] & 0xf);
+		++cell;
+		while (cell < column)
+		{
+			ft_putchar(' ');
+			print_bit((arena[cell] & 0xf0) >> 4);
+			print_bit(arena[cell] & 0xf);
+			++cell;
+		}
+		++i;
+		ft_putchar('\n');
+	}*/
 }
 
 void	init_arena(void)
 {
-	unsigned char ch;
-	unsigned char	*tmp;
-	unsigned short int *arena;
+	unsigned char	ch;
+	unsigned char	*arena;
 
-	arena = (unsigned short int *)malloc(MEM_SIZE);
-	tmp = (char *)arena;
-	arena[0] = 0xf0;
-	arena[1] = 0xf;
+	arena = (unsigned char *)malloc(MEM_SIZE);
 	
+	//TMP
+	arena[0] = 0x0f;
+	arena[1] = 0x12;
+	unsigned char high;
+	unsigned char low;
+	high = (arena[1] & 0xf0) >> 4;
+	low = arena[1] & 0xf;
+	//printf("%x%x\n", high, low);
 
-	unsigned short int kek = arena[0];
-	printf("%c\n", (unsigned char)kek % 10 + 48);
-	for (int i = 0; i < 8; i++)
-	{
-		printf("%d", kek % 2);
-		kek = kek / 2;
-	}
-
-	//printf("letter - %d\n", *tmp);
-	//printf("letter - %d\n", *(arena+1));
-	printf("\ninitializing arena\n");
-	printf("size - %zu\n", sizeof(char));
+	//print_row_label(35);
+	//END
+	//printf("initializing arena\n");
 	print_arena(arena);
 }
