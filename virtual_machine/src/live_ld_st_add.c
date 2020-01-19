e4bf0e34fa903e67d477e52354104ed9cde23350
Ben-Dudley <vilenaguslyakova@gmail.com>
2020-01-19 20:25:20 +0300

    merge

--- virtual_machine/src/live_ld_st_add.c
+++ virtual_machine/src/live_ld_st_add.c
@@ -14,20 +14,18 @@
 
 void		live_op(t_info *info, t_processes **prs, t_sdl *sdl)
 {
-	int     arg;
-	int     new_location;
+	int				number_player;
+	short int		new_location;
+	short int		current_location;
 
-    printf("live\n");
+	current_location = (*prs)->index;
 	(*prs)->cc_live = info->count_cycles;
-	arg = (info->arena)[((*prs)->index + 1) % MEM_SIZE];
-	if (arg == (*prs)->reg[0])
+	number_player = reverse_int(*((int *)((info->arena) + (current_location + 1) % MEM_SIZE)));
+	if (number_player == (*prs)->reg[0])
 		info->last_live = (*prs)->reg[0];
 	new_location = (((*prs)->index) + 5) % MEM_SIZE;
-	//create_cursor(info->arena[new_location], new_location, (*prs)->reg[0] - 1, sdl);
-	//update_byte(info->arena[(*prs)->index], (*prs)->index, (*prs)->reg[0] - 1, sdl);
 	move_cursor((*prs)->index, 5, (*prs)->reg[0] - 1, sdl);
-    (*prs)->index = new_location; //1 байт занимает код операции и 4 байта занимает аргумент
-	//printf("cc_live %d\n./", (*prs)->cc_live);
+    (*prs)->index = new_location;
 }
 
 void		ld_op(t_info *info, t_processes **prs, t_sdl *sdl)
