1af1c29e681b4adae74841ac7aeca79fec96eed8
Rachael Gyles <rgyles@at-r7.21-school.ru>
2020-02-09 16:15:20 +0300

    added data section clear before updating

--- virtual_machine/src/show_data.c
+++ virtual_machine/src/show_data.c
@@ -6,7 +6,7 @@
 /*   By: rgyles <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/14 17:49:29 by rgyles            #+#    #+#             */
-/*   Updated: 2020/02/08 13:54:17 by rgyles           ###   ########.fr       */
+/*   Updated: 2020/02/09 16:14:47 by rgyles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ void	udpate_delay(int delay, t_sdl *sdl)
 	render_info->rect.w = SDL_DATA_WIDTH;
 	render_info->font_color = sdl->colors[WHITE];
 	render_info->back_color = sdl->colors[BLACK];
-	SDL_FillRect(sdl->surface, &sdl->render_info->rect, 0);
 	if (delay == 0)
 		render_text("no delay", render_info, sdl->surface);
 	else
@@ -88,8 +87,11 @@ void	show_data(t_info *info, t_sdl *sdl)
 	render_info = sdl->render_info;
 	render_info->rect.x = DATA_X_LOCATION;
 	render_info->rect.y = DATA_Y_LOCATION;
+	render_info->rect.w = 150;
+	render_info->rect.h = 120;
 	render_info->font_color = sdl->colors[WHITE];
 	render_info->back_color = sdl->colors[BLACK];
+	SDL_FillRect(sdl->surface, &sdl->render_info->rect, 0);
 	update_value(info->count_cycles, render_info, sdl->surface);
 	sdl->render_info->rect.y += TEXT_Y_SHIFT;
 	update_value(info->last_live, render_info, sdl->surface);
